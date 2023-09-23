c03cf28f39ccdfec246633fc99348cc11483fb41
Magistra <magistraagis@gmail.com>
2023-09-23 15:48:50 +0800

    add property

--- app/components/MessageBox.js
+++ app/components/MessageBox.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function MessageBox() {
+export default function MessageBox({message}) {
   return (
     <div className='border border-black w-full p-4 rounded-md mb-2'>
         <h1 className='font-bold'>anonymous</h1>
-        <p>The message is here</p>
+        <p>{message}</p>
     </div>
   )
 }
